4bd19162936483e1e44f8ebfea2a4586bb752c0c
Shane Reustle <me@shanereustle.com>
2018-04-30 22:32:45 -0400

    added recipes templates

--- js/functions.js
+++ js/functions.js
@@ -3,6 +3,12 @@
 	var $doc = $(document);
 
 	$doc.ready(function() {
+
+		$('.accordion__head').on('click', function(e){
+			e.preventDefault();
+
+			$(this).siblings('.accordion__body').slideToggle().closest('.accordion').toggleClass('open');
+		});
 		
 	});
 
